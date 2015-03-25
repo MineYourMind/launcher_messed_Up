053437568bc72fe22775db950b56641e5c1d9c40
SirWilli <brieftaubenman@gmail.com>
2015-03-25 18:04:22 +0100

    Removes ugly hover border

--- launcher/src/main/java/com/skcraft/launcher/swing/LinkButton.java
+++ launcher/src/main/java/com/skcraft/launcher/swing/LinkButton.java
@@ -15,8 +15,7 @@ import java.awt.event.MouseEvent;
 public class LinkButton extends JButton {
 
     private static final Color LINK_COLOR = Color.blue;
-    private static final Border LINK_BORDER = BorderFactory.createEmptyBorder(0, 0, 1, 0); 
-    private static final Border HOVER_BORDER = BorderFactory.createMatteBorder(0, 0, 1, 0, LINK_COLOR);
+    private static final Border LINK_BORDER = BorderFactory.createEmptyBorder(0, 0, 1, 0);
     
     public LinkButton() {
         super();
@@ -51,11 +50,6 @@ public class LinkButton extends JButton {
         setRequestFocusEnabled(false); 
         setContentAreaFilled(false);
         addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                ((JComponent) e.getComponent()).setBorder(HOVER_BORDER);
-            }
-
             @Override
             public void mouseReleased(MouseEvent e) {
                 ((JComponent) e.getComponent()).setBorder(LINK_BORDER);
